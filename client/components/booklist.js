337ac5218e4acf10f964e4bf0d910ea78ee2d233
Caroline Tobin <ctobin25>
2021-04-20 20:12:01 -0400

    added file structure

---
+++ client/components/booklist.js
@@ -0,0 +1,14 @@
+
+import React, { useState}  from 'react'
+import { Link, useLocation } from "react-router-dom";
+
+function BookList(props) {
+    const location = useLocation();
+    const messageStyle = {
+        // width: '50%',
+        position: 'sticky',
+        top: '0',
+        left: '0'
+    }
+}
+
