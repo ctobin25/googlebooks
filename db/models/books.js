79127777ea74e8b5d7a51b72b7f670d5e24b9a30
Caroline Tobin <ctobin25>
2021-04-21 19:28:53 -0400

    added mongo schema

--- db/models/books.js
+++ db/models/books.js
@@ -1,2 +1,14 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+
+
+let books = new Schema ({
+    bookId :  { type: String, trim: true },
+    title :  { type: String, trim: true },
+    thumbnail :  { type: String },
+    link :  { type: String }
+}, {
+   timestamps: true 
+});
+
+module.exports = mongoose.model('books', books);
