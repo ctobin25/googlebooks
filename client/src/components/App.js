0ab5a682c30843f6541a863f5d0ee572ec903275
Caroline Tobin <ctobin25>
2021-04-23 12:29:43 -0400

    styling

---
+++ client/src/components/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+
+import NavBar from './components/NavBar';
+import SearchPage from './components/SearchPage';
+import SavedPage from './components/SavedPage';
+
+
+function App() {
+  return (
+    <Router>
+      <NavBar/>
+        <Route exact path= {["/", "/search", "/google_books_search"]}  component={SearchPage} />
+        <Route exact path= "/saved"  component={SavedPage} />
+    </Router>
+  );
+}
+
+export default App;
