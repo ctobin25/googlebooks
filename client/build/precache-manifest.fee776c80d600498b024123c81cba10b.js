d0590f258e2a44e4f9b42e322948614267ad23e3
Caroline Tobin <ctobin25>
2021-04-26 18:31:20 -0400

    update'

---
+++ client/build/precache-manifest.fee776c80d600498b024123c81cba10b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c2245a43805f52f245353219013e4ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a26eaa871e599685f238",
+    "url": "/static/css/main.6cf4a936.chunk.css"
+  },
+  {
+    "revision": "19fdf46ecd10acf3ef39",
+    "url": "/static/js/2.7eff124b.chunk.js"
+  },
+  {
+    "revision": "9be8f09f0bfe1d20164d4c42e389b466",
+    "url": "/static/js/2.7eff124b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a26eaa871e599685f238",
+    "url": "/static/js/main.a4f7fe11.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
